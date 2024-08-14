6b19c836518ae6538d48a96be8cb14bd63fb64c8
Pradeep Ram Polaki <pradeeprampolaki@gmail.com>
2024-08-14 07:38:25 +0530

    portfolio website
    nananan

---
+++ src/data/constants.js
@@ -0,0 +1,238 @@
+
+import spidy from "../images/spidy.jpg"
+import logo from "../images/spidy logo.jpg"
+
+
+export const Bio = {
+  name: "Pradeep Ram Polaki",
+  roles: [
+    "Frontend developer",
+    "Python Developer",
+    "AI/ML Developer",
+  ],
+  description:
+    "Final-year B.Tech student in Artificial Intelligence and Data Science, with a flair for frontend magic, AI/ML wizardry, and Python prowess. Renowned for blending smart work with unwavering dedication, I'm ready to push the boundaries of innovation and make a mark in the tech world.",
+  github: "https://github.com/pradeep-1817",
+  resume:
+    "https://drive.google.com/file/d/17bcOztBUQ9vF-e0CEb0ZYB-ljPB1qReu/view?usp=drive_link",
+  linkedin: "linkedin.com/in/polaki-pradeep-ram-591956293/",
+  insta: "https://www.instagram.com/_.pradeepram._/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node.js",
+        image:
+          "https://banner2.cleanpng.com/20180425/jrw/ave9tlfdy.webp",
+      },
+      {
+        name: "Express",
+        image: "https://w7.pngwing.com/pngs/846/87/png-transparent-mean-solution-stack-express-js-node-js-javascript-github-text-trademark-logo-thumbnail.png",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Postgresql",
+        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+      },
+      {
+        name: "MongoDB",
+        image: "https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "AI/ML",
+    skills: [
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+      {
+        name: "Numpy",
+        image:
+            "https://icon.icepanel.io/Technology/svg/Matplotlib.svg"      
+      },
+      {
+        name: "Algorithms",
+        image:
+          "https://cdn-icons-png.freepik.com/512/15237/15237417.png?ga=GA1.1.119099582.1712822957",
+      },
+      {
+        name: "ML models",
+        image:
+          "https://cdn-icons-png.freepik.com/256/2172/2172891.png?semt=ais_hybrid",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://git-scm.com/images/logo@2x.png",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQ7WL31BgP94lXiVCt-m6cReSz8AqGnKVy4hCWa-kh_qEEFDB6byi_OfMP&s=10",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "ChatGPT",
+        image:
+            "https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/chatgpt-icon.png"
+      }
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkcKbiMxBCzK3tmdk7GrqzjSliV0oiODmFrg&s",
+    role: "Data engineer associate",
+    company: "Blackoffer",
+    date: "",
+    desc: "Contributed on AI/ML, Python and Web based projects.Worked as an intern for few months where i learned a lot on real time projects Data visualization,Ml models.",
+    skills: [
+      "AI",
+      "ML",
+      "data visualization",
+      "Python",
+      "web scraping"
+
+    ],
+    doc: "",
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4D0BAQEdMKvk2YwVGw/company-logo_200_200/0/1652036458062/mount_litera_zee_school___india_logo?e=1728518400&v=beta&t=ZThJrRZMMueC-ghuUuyixB6j_3rLhedmZoBcgIWO09E",
+    school: "Raghu Institute of technology",
+    date: "2021 - 2025",
+    grade: "8.15 CGPA",
+    desc: "I am currently pursuing B.Tech Computer Science and Engineering at Raghu Institute of Technology. I have completed 6 semesters and have a CGPA of 8.15. I have taken courses in Reactjs, Web Development, Machine Learning,Data visualization.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/C4D0BAQEdMKvk2YwVGw/company-logo_200_200/0/1652036458062/mount_litera_zee_school___india_logo?e=1728518400&v=beta&t=ZThJrRZMMueC-ghuUuyixB6j_3rLhedmZoBcgIWO09E",
+    school: "Narayana Jr college",
+    date: "2019 - 2021",
+    grade: "92.9%",
+    desc: "I completed my class 12 high school education at Narayana Jr college, where I studied M P c.",
+    degree: "XII, M P C",
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/C4D0BAQEdMKvk2YwVGw/company-logo_200_200/0/1652036458062/mount_litera_zee_school___india_logo?e=1728518400&v=beta&t=ZThJrRZMMueC-ghuUuyixB6j_3rLhedmZoBcgIWO09E",
+    
+    school: "Bharathi E M High School",
+    date: "Apr 2018 - Apr 2019",
+    grade: "91.5%",
+    desc: "I completed my class 10 education at Bharathi E M High School.",
+    degree: "SSC X",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "YET TO COME Sentimental analysis",
+    date: "---",
+    description:
+      "Will be updated when fully developed currently under process.",
+    image:
+      logo,
+    tags: [
+      "Python",
+      "Natural Language Processing",
+      "ML",
+      "API",
+      "OpenAI"
+    ],
+    category: "Machine Learning",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Meee",
+        img: spidy,
+        linkedin: "linkedin.com/in/polaki-pradeep-ram-591956293/",
+        github: "https://github.com/pradeep-1817/",
+      }
+    ],
+  },
+  {
+    id: 2,
+    title: "YET TO COME Medium Clone",
+    date: "---",
+    description:
+      "Will be updated when fully developed currently under process.",
+    image:
+      logo,
+    tags: [
+      "JavaScript",
+      "React",
+      "Tailwind",
+      "Hono",
+    ],
+    category: "Web",
+    github: "",
+    webapp: "",
+    member: [
+      {
+        name: "Meee",
+        img: spidy,
+        linkedin: "linkedin.com/in/polaki-pradeep-ram-591956293/",
+        github: "https://github.com/pradeep-1817/",
+      }
+    ],
+  },
+];
+
+
